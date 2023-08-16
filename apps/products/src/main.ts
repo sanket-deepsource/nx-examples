225c8c03ef590801ff0d150da2862fcaa86cb7c7
Sanket Saurav <sanket@deepsource.io>
2023-08-16 16:27:51 +0530

    Update main.ts

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const STRIPE_API_KEY="sk_cndscnsdkc432nbjkcnsdkcnsdkn"
+
 if (environment.production) {
   enableProdMode();
 }
